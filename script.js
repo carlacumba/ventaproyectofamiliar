b987a45fb3d7380461bc8edbbb86fae1dcef1883
carlacumba <carlitanicole2007@gmail.com>
2025-12-16 16:34:56 -0500

    Detalle

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const venta = [
     category: "Muebles",
     price: 600,
     estado: "Bueno",
-    img: "imagenesventa/comedor.png", // Borré el ".."
+    img: "imagenesventa/comedor.PNG", // Borré el ".."
     desc: `Juego de comedor de 6 sillas moderno hexagonal con tablero de vidrio central y base con espacio de almacenamiento.`,
   },
   {
